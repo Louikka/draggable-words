b822e6a80197948d059c78fcec5577818928cf3a
Louikka <lolikadimonaa@gmail.com>
2026-04-30 23:09:25 +0300

    added html minifier
    + some minor style changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,24 @@
 import { defineConfig } from 'vite';
+import htmlMinifier from 'vite-plugin-html-minifier';
 
 
 export default defineConfig({
     base : '',
+    plugins : [
+        htmlMinifier({
+            minify : {
+                collapseWhitespace: true,
+                keepClosingSlash: false,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: false,
+                removeStyleLinkTypeAttributes: false,
+                removeEmptyAttributes: true,
+                useShortDoctype: true,
+                minifyCSS: true,
+                minifyJS: true,
+                minifyURLs: true,
+            },
+        }),
+    ],
 });
